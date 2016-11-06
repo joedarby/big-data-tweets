079905eeb5f7cae77dc87af1bcad32f6c27a46fd
joedarby <joedarby87@gmail.com>
2016-11-06 22:23:19 +0000

    create hashtags program

---
+++ tweet-hashtags/src/HashtagsJob.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class HashtagsJob {
+    public static void runJob(String[] input, String output) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance();
+        job.setJarByClass(HashtagsJob.class);
+  
+        job.setReducerClass(HashtagsReducer.class);
+        job.setMapperClass(HashtagsMapper.class);
+        
+        job.setOutputKeyClass(Text.class);
+        
+        job.setOutputValueClass(IntWritable.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+	job.setNumReduceTasks(3);
+        
+        Path outputPath = new Path(output);
+        FileInputFormat.setInputPaths(job, StringUtils.join(input, ","));
+        FileOutputFormat.setOutputPath(job, outputPath);
+        outputPath.getFileSystem(conf).delete(outputPath, true);
+        job.waitForCompletion(true);
+        }
+    public static void main(String[] args) throws Exception {
+        runJob(Arrays.copyOfRange(args, 0, args.length - 1), args[args.length - 1]);
+    }
+}
