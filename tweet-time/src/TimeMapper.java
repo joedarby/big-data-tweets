a288da4b315ce63b6ea24ecc18c9d1406a5e7c50
joedarby <joedarby87@gmail.com>
2016-11-06 19:57:44 +0000

    use simple date format

--- tweet-time/src/TimeMapper.java
+++ tweet-time/src/TimeMapper.java
@@ -1,14 +1,12 @@
 import java.io.IOException;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import static java.util.Calendar.DAY_OF_MONTH;
-import static java.util.Calendar.MONTH;
-import static java.util.Calendar.YEAR;
 
 public class TimeMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
@@ -21,9 +19,9 @@ public class TimeMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
             try {
                 long tweetTime = Long.parseLong(splitLine[0]);
-                Calendar cal = Calendar.getInstance();
-                cal.setTimeInMillis(tweetTime);
-                String dateString = cal.get(DAY_OF_MONTH) + "/" + cal.get(MONTH) + "/" + cal.get(YEAR);
+                Date tweetSimpleDate = new Date(tweetTime);
+                SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+                String dateString = format.format(tweetSimpleDate);
 
                 Text tweetDate = new Text();
                 tweetDate.set(dateString);
