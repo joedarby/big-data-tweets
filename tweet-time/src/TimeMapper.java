119a5277df864b277b7933677a3797f7f0b72aa9
joedarby <joedarby87@gmail.com>
2016-11-06 18:43:00 +0000

    create tweet-time

---
+++ tweet-time/src/TimeMapper.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class TimeMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	@Override    
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+	    String[] splitLine = value.toString().split(";");
+
+        if (splitLine.length == 4) {
+
+            long tweetTime = Long.parseLong(splitLine[0]);
+            Date tweetSimpleDate = new Date(tweetTime);
+            String dateString = tweetSimpleDate.getDate() + "/" + tweetSimpleDate.getMonth() + "/" + tweetSimpleDate.getYear();
+
+            Text tweetDate = new Text();
+            tweetDate.set(dateString);
+
+            IntWritable one = new IntWritable(1);
+
+            context.write(tweetDate, one);
+        }
+
+    }
+        
+    	
+    	
+}
