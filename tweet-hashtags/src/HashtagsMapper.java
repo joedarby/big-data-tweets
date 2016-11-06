3ba5371dad70b30a3aa91e9125c40592d6641b26
joedarby <joedarby87@gmail.com>
2016-11-06 23:04:27 +0000

    try pattern matcher again

--- tweet-hashtags/src/HashtagsMapper.java
+++ tweet-hashtags/src/HashtagsMapper.java
@@ -13,8 +13,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class HashtagsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-    //private ArrayList<String> hashtags = new ArrayList<>();
-
 	@Override    
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
@@ -24,6 +22,24 @@ public class HashtagsMapper extends Mapper<LongWritable, Text, Text, IntWritable
 
             String tweet = splitLine[2];
 
+            if (tweet.contains("#")) {
+                ArrayList<String> hashtags = new ArrayList<>();
+                Pattern myRegex = Pattern.compile("(\\s|\\A)#([\\w\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u01FF]+)");
+                Matcher matcher = myRegex.matcher(tweet);
+
+                while (matcher.find()) {
+                    hashtags.add("#" + matcher.group());
+                }
+
+                for (String hashtag : hashtags) {
+                    Text textTag = new Text();
+                    textTag.set(hashtag);
+                    IntWritable one = new IntWritable(1);
+                    context.write(textTag, one);
+                }
+
+            }
+            /*
             String[] hashtags = tweet.split("#");
 
             if (hashtags.length > 1) {
@@ -38,19 +54,7 @@ public class HashtagsMapper extends Mapper<LongWritable, Text, Text, IntWritable
                 }
 
             }
-
-            /*Pattern myRegex = Pattern.compile("(\\s|\\A)#([\\w\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u01FF]+)");
-            Matcher matcher = myRegex.matcher(tweet);
-
-            while (matcher.find()) {
-                hashtags.add("#" + matcher.group());
-            }
             */
-
-
-
-
-
 
         }
 
