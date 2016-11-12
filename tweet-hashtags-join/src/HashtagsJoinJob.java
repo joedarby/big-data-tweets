bd18b730c5dcf8fa6414b961ef42ba8a681a1084
joedarby <joedarby87@gmail.com>
2016-11-12 18:25:46 +0000

    create hashtag join

---
+++ tweet-hashtags-join/src/HashtagsJoinJob.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class HashtagsJoinJob {
+    public static void runJob(String[] input, String output) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance();
+        job.setJarByClass(HashtagsJoinJob.class);
+        job.setMapperClass(HashtagsJoinMapper.class);
+        //no reducer - map only job
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(TextIntPair.class);
+
+        job.addCacheFile(new Path("/input/hashtags.tsv").toUri());
+
+	    job.setNumReduceTasks(3);
+        
+        Path outputPath = new Path(output);
+        FileInputFormat.setInputPaths(job, StringUtils.join(input, ","));
+        FileOutputFormat.setOutputPath(job, outputPath);
+        outputPath.getFileSystem(conf).delete(outputPath, true);
+        job.waitForCompletion(true);
+        }
+    public static void main(String[] args) throws Exception {
+        runJob(Arrays.copyOfRange(args, 0, args.length - 1), args[args.length - 1]);
+    }
+}
---
+++ tweet-hashtags-join/src/HashtagsJoinMapper.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.Hashtable;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class HashtagsJoinMapper extends Mapper<Text, IntWritable, Text, TextIntPair> {
+
+	private Hashtable<String, String> countryCodes;
+	private TextIntPair countryTotalPair = new TextIntPair();
+	
+	//Map takes a "key = hashtag" and "value = occurrence count" pair from the tweets-hashtags job
+	public void map(Text key, IntWritable value, Context context) throws IOException, InterruptedException {
+
+		String hashtag = key.toString();
+
+		//Look at hashtag to see if it contains an IOC country code or other relevant string (eg. "TEAMGB")
+		int matches = 0;
+		String country = "none";
+		for (String code : countryCodes.keySet()) {
+			if (hashtag.contains(code)) {
+				matches += 1;
+				if (matches > 1) {
+					country = "none";	// If more than one country code (or other match) is made, set country = none (to exclude eg. "GBRvsFRA")
+					break;
+				} else {
+					country = countryCodes.get(code);   // If exactly one match is made, assign this hashtag to this country.
+				}
+			}
+		}
+
+		countryTotalPair.set(country, value.get());
+
+		// Emit all of the original hashtags, but now labelled with the country they are *probably* supporting
+		context.write(key, countryTotalPair);
+
+	}
+
+	@Override
+	protected void setup(Context context) throws IOException, InterruptedException {
+
+		countryCodes = new Hashtable<String, String>();
+
+		URI fileUri = context.getCacheFiles()[0];
+		FileSystem fs = FileSystem.get(context.getConfiguration());
+		FSDataInputStream in = fs.open(new Path(fileUri));
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+		String line = null;
+		try {
+			// we discard the header row
+			br.readLine();
+
+			while ((line = br.readLine()) != null) {
+				context.getCounter(CustomCounters.NUM_COUNTRYCODES).increment(1);
+
+				String[] fields = line.split("\t");
+				if (fields.length == 2)
+					countryCodes.put(fields[0], fields[1]);
+			}
+			br.close();
+		} catch (IOException e1) {
+		}
+
+		super.setup(context);
+	}
+
+}
